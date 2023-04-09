58c5ac607f464a12182249088c899ea9e9887e96
Gate Way <gwnt@azerbaijansupermarket.com>
2023-04-09 01:24:00 -0700

    initial commit

---
+++ src/settings.ts
@@ -0,0 +1,19 @@
+"use strict";
+
+import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+import powerbi from "powerbi-visuals-api";
+import Fill = powerbi.Fill;
+
+export class VisualSettings extends DataViewObjectsParser {
+  // public dataPoint: dataPointSettings = new dataPointSettings();
+  public barchartProperties: BarchartProperties = new BarchartProperties();
+}
+
+export class BarchartProperties {
+  sortBySize: boolean = true;
+  xAxisFontSize: number = 10;
+  yAxisFontSize: number = 10;
+  positiveBarColor: Fill = { "solid": { "color": "#40916C" } }; // default color is  teal;
+  negativeBarColor: Fill = { "solid": { "color": "#EF233C" } }; // default color is  teal;
+}
